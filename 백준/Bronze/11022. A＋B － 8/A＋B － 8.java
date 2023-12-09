ca23f4f826acbd271e624539fda35fa6e7a2156c
rktndnjseh <150515419+rktndnjseh@users.noreply.github.com>
2023-12-09 10:42:03 +0900

    [Bronze V] Title: A+B - 8, Time: 176 ms, Memory: 16436 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,21 @@
+//https://www.acmicpc.net/problem/11022
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        T=T+1;
+        for(int i=1;i<T;i++) {
+            String [] input =br.readLine().split(" ");
+            int A = Integer.parseInt(input[0]);
+            int B = Integer.parseInt(input[1]);
+            bw.write("Case #"+i+": "+A +" + "+ B +" = "+(A+B)+"\n");
+        }
+        bw.flush();
+        bw.close();
+}
+}
