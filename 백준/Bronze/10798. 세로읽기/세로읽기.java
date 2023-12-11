a7698a75c852bd3f94f8fb53c0f926cac91370bc
rktndnjseh <150515419+rktndnjseh@users.noreply.github.com>
2023-12-11 11:25:09 +0900

    [Bronze I] Title: 세로읽기, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        char[][] arr = new char[15][5];
+        for (int i = 0; i < 5; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < line.length(); j++) {
+                arr[j][i] = line.charAt(j);
+            }
+        }
+
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (arr[i][j] != 0) {
+                    bw.write(arr[i][j]);
+                }
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+}
