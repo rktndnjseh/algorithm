d364b08728de9b115301ee19cb40e062027e2c86
rktndnjseh <150515419+rktndnjseh@users.noreply.github.com>
2023-12-11 15:56:16 +0900

    [Gold IV] Title: 오큰수, Time: 1072 ms, Memory: 142636 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,43 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int[] array = new int[N];
         int[] answer = new int[N];
-
+        int[] array = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < N; i++) {
             array[i] = Integer.parseInt(st.nextToken());
         }
-
         calculateStack(N, array, answer);
         printArray(answer);
     }
-
     public static void calculateStack(int N, int[] array, int[] answer) {
         Stack<Integer> stack = new Stack<>();
-        Arrays.fill(answer, -1);
-        for (int i = 0; i < N; i++) {
-            while (!stack.empty() && array[stack.peek()] < array[i]) {
-                answer[stack.pop()] = array[i];
+           for (int i = N - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= array[i]) {
+                stack.pop();
             }
-            stack.push(i);
+            answer[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(array[i]);
         }
     }
-
-    public static void printArray(int[] answer) {
-        StringBuilder sb = new StringBuilder();
-        for (int num : answer) {
-            sb.append(num).append(" ");
-        }
-        System.out.println(sb);
-    }
+   public static void printArray(int[]answer){
+       StringBuilder sb = new StringBuilder();
+       for (int ans:answer) {
+           sb.append(ans).append(" ");
+       }
+       System.out.println(sb);
+   }
 }
