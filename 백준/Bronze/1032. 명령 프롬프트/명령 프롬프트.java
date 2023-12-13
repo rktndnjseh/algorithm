1eb9b03a27f508f75d5c08e9caf56a7d1aed92ea
rktndnjseh <150515419+rktndnjseh@users.noreply.github.com>
2023-12-13 18:50:44 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 124 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
@@ -1,31 +1,21 @@
 import java.io.*;
-
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        char[] pattern = br.readLine().toCharArray();
-        StringBuilder sb = new StringBuilder(new String(pattern)); // StringBuilder로 변환
+        int N = Integer.parseInt(br.readLine());
+        String pattern = br.readLine();
+        StringBuilder sb = new StringBuilder(pattern);
 
-        char[][] filenames = new char[n-1][];
-        for(int i = 0; i < n-1; i++) {
-            filenames[i] = br.readLine().toCharArray();
-        }
-
-        for(char[] fileName : filenames) {
-            int j = 0;
-            for(char patternChar : pattern) {
-                if(!Character.valueOf(patternChar).equals(fileName[j]) && sb.charAt(j) != '?') {
-                    sb.setCharAt(j, '?'); // 패턴이 변경되는 경우에만 수정
+        while (N-- > 1) {
+            String fileName = br.readLine();
+            for (int i = 0; i < pattern.length(); i++) {
+                if (pattern.charAt(i) != fileName.charAt(i)) {
+                    sb.setCharAt(i, '?');
                 }
-                j++;
             }
         }
 
-        bw.write(sb.toString()); // StringBuilder를 문자열로 변환하여 출력
-        bw.flush();
-        bw.close();
+ System.out.println(sb);
     }
 }
