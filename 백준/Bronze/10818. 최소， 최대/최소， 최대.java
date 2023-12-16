52ff839d0119dfc2acc5365aab29f933a0a04c7e
rktndnjseh <150515419+rktndnjseh@users.noreply.github.com>
2023-12-16 22:18:27 +0900

    [Bronze III] Title: 최소, 최대, Time: 524 ms, Memory: 92080 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] num = new int[N];
+
+        int max = -1000001;
+        int min = 1000001;
+        for(int i=0; i<N; i++){
+            num[i] = Integer.parseInt(st.nextToken());
+            if(max<num[i])max=num[i];
+            if(min>num[i])min=num[i];
+        }
+        bw.write(min+" "+max);
+        bw.flush();
+        bw.close();
+    }
+}
