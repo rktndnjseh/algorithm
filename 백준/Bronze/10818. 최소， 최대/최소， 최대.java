136837de9d00d93e604dfa650d02c379d97a5cac
rktndnjseh <150515419+rktndnjseh@users.noreply.github.com>
2023-12-17 00:10:53 +0900

    [Bronze III] Title: 최소, 최대, Time: 188 ms, Memory: 31648 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,24 +1,36 @@
-import java.io.*;
-import java.util.StringTokenizer;
+interface Main {
+    int InputSize=9000012;
+    int[]index={0};
+    byte[] inputBuffer = new byte[InputSize+1];;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static void main(String[] args) throws Exception {
+         System.in.read(inputBuffer);
 
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] num = new int[N];
+        int Num;
+        int N = read();
 
         int max = -1000001;
         int min = 1000001;
-        for(int i=0; i<N; i++){
-            num[i] = Integer.parseInt(st.nextToken());
-            min=Math.min(min, num[i]);
-            max=Math.max(max, num[i]);
+        for(int i=0;i<N;i++)
+        {
+            Num=read();
+            max=Math.max(max, Num);
+            min=Math.min(min, Num);
         }
-        bw.write(min+" "+max);
-        bw.flush();
-        bw.close();
+        System.out.println(min+" "+max);
+    }
+    //아스키코드로 숫자를 하나하나 입력받아서 정수 하나로 만들어서 반환하는 함수
+    static int read(){
+        int ASCII, result=0, isMinus=1;
+        while((ASCII=inputBuffer[index[0]++])>32){
+            //줄바꿈, 탭 등의 공백문자를 만날때까지
+            if(ASCII==45){//"-"문자인지 확인, 음수인지 확인
+                isMinus=-1;
+            }else result=result*10+ASCII-48;
+        //ASCII코드에서 0은 48이다.48을 빼면 원래 숫자가 된다.
+        //result*10은 이전까지 읽어들인 숫자를 한자리 수 뒤로 밀어준다.
+        //그 후 +ASCII-48로 새로 읽은 숫자를 더한다.
+        }
+        return result*isMinus;
     }
 }
