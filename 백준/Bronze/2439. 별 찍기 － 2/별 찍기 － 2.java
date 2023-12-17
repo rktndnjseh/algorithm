2ce1ec0fc00ecedcd2adb10aec25575ff82c7c79
rktndnjseh <150515419+rktndnjseh@users.noreply.github.com>
2023-12-17 18:02:27 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 144 ms, Memory: 23116 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,10 +1,28 @@
-import java.util.*;
-public class Main {
-    public static void main(String[] args){
-        Scanner s = new Scanner(System.in);
-        int N = s.nextInt();
-        for(int i=0; i<N; i++){
+interface Main {
+    int InputSize=9000012;
+    int[]index={0};
+    byte[] inputBuffer = new byte[InputSize+1];;
+
+    static void main(String[] args) throws Exception {
+        System.in.read(inputBuffer);
+
+        int N = read();
+
+         for(int i=0;i<N;i++)
+        {
             System.out.println(" ".repeat(N-i-1) + "*".repeat(i+1));
         }
+    }static int read(){
+    int ASCII, result=0, isMinus=1;
+    while((ASCII=inputBuffer[index[0]++])>32){
+        //줄바꿈, 탭 등의 공백문자를 만날때까지
+        if(ASCII==45){//"-"문자인지 확인, 음수인지 확인
+            isMinus=-1;
+        }else result=result*10+ASCII-48;
+        //ASCII코드에서 0은 48이다.48을 빼면 원래 숫자가 된다.
+        //result*10은 이전까지 읽어들인 숫자를 한자리 수 뒤로 밀어준다.
+        //그 후 +ASCII-48로 새로 읽은 숫자를 더한다.
     }
+    return result*isMinus;
+}
 }
