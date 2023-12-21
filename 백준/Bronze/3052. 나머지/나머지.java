4d202635eb99f6b8e12f423c55a563df1f70098e
rktndnjseh <150515419+rktndnjseh@users.noreply.github.com>
2023-12-21 14:13:20 +0900

    [Bronze II] Title: 나머지, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -2,23 +2,12 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int copy = -1, count = 0;
-        int[] array = new int[10];
+        Scanner s = new Scanner(System.in);
+        Set<Integer> set = new HashSet<>();
 
-        for(int i = 0; i < 10; i++) {
-            array[i] = sc.nextInt() % 42;
-        }
-        sc.close();
-        Arrays.sort(array);
+        for(int i = 0; i < 10; i++) set.add(s.nextInt() % 42);
+        s.close();
 
-        for (int i = 0; i < 10; i++) {
-            if (copy != array[i]) {
-                copy = array[i];
-                count++;
-            }
-        }
-
-        System.out.println(count);
+        System.out.println(set.size());
     }
 }
