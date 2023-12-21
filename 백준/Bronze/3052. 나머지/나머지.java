91ab56a0b89a1ede5fa42825f26017acadf7658b
rktndnjseh <150515419+rktndnjseh@users.noreply.github.com>
2023-12-21 14:57:27 +0900

    [Bronze II] Title: 나머지, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,12 +1,14 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Set<Integer> set = new HashSet<>();
 
-        for(int i = 0; i < 10; i++) set.add(s.nextInt() % 42);
-        s.close();
+        for(int i = 0; i < 10; i++) set.add(Integer.parseInt(br.readLine()) % 42);
 
         System.out.println(set.size());
     }
