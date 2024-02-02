c72cc6c8eb9b23f12080d2916375545947cfb262
rktndnjseh <150515419+rktndnjseh@users.noreply.github.com>
2024-02-02 09:49:33 +0900

    [Silver IV] Title: 상근이의 여행, Time: 76 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int T;
+
+    cin >> T;
+    while (T-- > 0) {
+        int N, M;
+        cin >> N >> M;
+        int a, b;
+        while (M-- > 0) {
+            cin >> a >> b;  
+        }
+        cout << N - 1 << endl;
+    }
+    return 0;
+}
