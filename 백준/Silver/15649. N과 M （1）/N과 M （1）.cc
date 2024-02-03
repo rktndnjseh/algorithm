860ebaa4b43b442adfc679cdb50cb8b5f9d96989
rktndnjseh <150515419+rktndnjseh@users.noreply.github.com>
2024-02-03 16:36:22 +0900

    [Silver III] Title: N과 M (1), Time: 36 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int N, M;
+int arr[9];
+bool visited[9];
+
+void dfs(int cnt) {
+	if (cnt == M) {
+		for (int i = 0; i < M; i++)
+			cout << arr[i] << ' ';
+		cout << '\n';
+		return;
+	}
+	for (int i = 1; i <= N; i++)
+	{
+		if (visited[i])continue;
+			visited[i] = true;
+			arr[cnt] = i;
+			dfs(cnt + 1);
+			visited[i] = false;
+	}
+}
+int main() {
+	cin >> N >> M;
+	dfs(0);
+
+	return 0;
+}
