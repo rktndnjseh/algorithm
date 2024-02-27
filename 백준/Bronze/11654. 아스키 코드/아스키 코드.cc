5768d97a4e90b8ead063f1e7a1910811268a7c4d
rktndnjseh <150515419+rktndnjseh@users.noreply.github.com>
2024-02-27 20:35:36 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    char N;
+    cin >> N;
+    cout << (int)N;
+        
+    return 0;
+}
