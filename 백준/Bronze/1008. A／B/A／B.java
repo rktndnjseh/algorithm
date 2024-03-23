33a1ec54221d386b7a854482ff015ab6115c4b5c
rktndnjseh <150515419+rktndnjseh@users.noreply.github.com>
2024-03-23 11:12:58 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 18048 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] ars) {
+    Scanner sc = new Scanner(System.in);
+
+    double a= sc.nextDouble(), b = sc.nextDouble();
+        System.out.print(a/b);
+}
+}
